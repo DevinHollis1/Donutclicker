45063b2294600c156a0a299dd40acd4063535ecd
Devin Hollis <hollis.devin23@gmail.com>
2023-06-16 02:54:24 -0400

    Auto clicks/reset

--- autoclicker.js
+++ autoclicker.js
@@ -1,9 +1,13 @@
 let autoClickerCount = 0;
-let autoClickerCost = 100;
+let autoClickerCost = 10;
+let clicksToPurchase = 10;
 
 
-const autoClicker = document.querySelector('#second-button button');
+
+const autoClicker = document.querySelector('#second-button > button');
 const autoClickerElement = document.querySelector('#second-button h1')
+const clicksToPurchaseElement = document.getElementById("clicksToPurchase");
+const resetButton = document.getElementById("reset-button");
 const autoClickerInterval = 1000;
 
 
@@ -18,28 +22,67 @@ function controlAutoClickerCount() {
     console.log(`${autoClickerCount} Auto Bakers`)
 }
 
+
 function controlAutoClickerClick(){
     if (donutCount >= autoClickerCost){
         donutCount -= autoClickerCost;
         autoClickerCount++;
         autoClickerCost = calculateAutoClickerCost();
+        controlAutoClicks();
         updateDonutCount();
-        updateAutoClickerCount()
+        updateAutoClickerCount();
+        updateAutoClickerButton();
         console.log(`purchased Auto Baker! donut count: ${donutCount}`);
     }
 }
 
 function calculateAutoClickerCost() {
     return Math.floor( autoClickerCost * 1.1);
-    
 }
+
+
+function updateDisplay() {
+    clicksToPurchaseElement.textContent = `Click ${autoClickerCost} Times for Auto Baker!`
+}
+
+function controlAutoClicks(){
+        clicksToPurchase = calculateClicksToPurchase();
+        updateDisplay();
+    }
+
+
+
+function calculateClicksToPurchase() {
+        return autoClickerCost;
+    }
+
 
 function incrementDonutCount() {
     donutCount += autoClickerCount;
+    controlAutoClicks();
     updateDonutCount();
 }
+
 setInterval(incrementDonutCount, autoClickerInterval);
 
+function updateAutoClickerButton() {
+    autoClicker.disabled = donutCount < autoClickerCost;
+}
 
-autoClicker.addEventListener('click', controlAutoClickerClick,)
+
+function resetPage() {
+    autoClickerCount = 0;
+    autoClickerCost = 10;
+    clicksToPurchase = 10;
+    donutCount = 0;
+    updateAutoClickerCount();
+    updateDisplay();
+    updateDonutCount();
+    updateAutoClickerButton();
+}
+
+
+
+resetButton.addEventListener('click', resetPage);
+autoClicker.addEventListener('click', controlAutoClickerClick)
 
--- donutclicker.js
+++ donutclicker.js
@@ -1,13 +1,26 @@
 let donutCount = 0;
 
 
+
+
 const donutCountElement = document.querySelector('.content h1');
+const donutCountElement2 = document.getElementById("donutCount1");
 const donutMaker = document.querySelector('.bake-buttons button');
 
 
 function updateDonutCount() {
     donutCountElement.textContent = ` Donuts ${donutCount}`;
+    donutCountElement2.textContent = donutCount;
+    updateAutoClickerButton();
 }
+
+
+
+function incrementDonutCount() {
+    donutCount++;
+    updateDonutCount();
+}
+
 
 
 function controlDonutMakerClick() {
