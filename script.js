d5f45e5bd44f7c2be015c381e478d5cdd5846266
Devin Hollis <hollis.devin23@gmail.com>
2023-06-16 14:01:41 -0400

    dropdown adjust

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 function toggleAboutDropdown(event) {
     const dropdownContainer = document.getElementById('dropdown-container');
     const aboutDropdown = document.getElementById('about-dropdown');
+    const inspirationDropdown = document.getElementById('inspiration-dropdown');
+    const contactsDropdown = document.getElementById('contacts-box');
+
     aboutDropdown.classList.toggle('show');
-    dropdownContainer.classList.toggle('show',aboutDropdown.classList.contains('show'));
-
+    dropdownContainer.classList.toggle('show', aboutDropdown.classList.contains('show'));
+    inspirationDropdown.classList.remove('show');
+    contactsDropdown.classList.remove('show');
 }
-
 
 function toggleInspirationDropdown(event) {
     const dropdownContainer = document.getElementById('dropdown-container');
-    const  inspirationDropdown = document.getElementById('inspiration-dropdown');
+    const inspirationDropdown = document.getElementById('inspiration-dropdown');
+    const aboutDropdown = document.getElementById('about-dropdown');
+    const contactsDropdown = document.getElementById('contacts-box');
+
     inspirationDropdown.classList.toggle('show');
-    dropdownContainer.classList.toggle('show',inspirationDropdown.classList.contains('show'));
+    dropdownContainer.classList.toggle('show', inspirationDropdown.classList.contains('show'));
+    aboutDropdown.classList.remove('show');
+    contactsDropdown.classList.remove('show');
 }
 
 function toggleContactsDropdown(event) {
     const dropdownContainer = document.getElementById('dropdown-container');
     const contactsDropdown = document.getElementById('contacts-box');
+    const aboutDropdown = document.getElementById('about-dropdown');
+    const inspirationDropdown = document.getElementById('inspiration-dropdown');
+
     contactsDropdown.classList.toggle('show');
     dropdownContainer.classList.toggle('show', contactsDropdown.classList.contains('show'));
-
+    aboutDropdown.classList.remove('show');
+    inspirationDropdown.classList.remove('show');
 }
 
-
-
-document.getElementById('about-dropdown').addEventListener('click', toggleAboutDropdown,)
-document.getElementById('inspiration-dropdown').addEventListener('click', toggleInspirationDropdown,)
-document.getElementById('contacts-box').addEventListener('click', toggleContactsDropdown,)
-
+document.getElementById('about-dropdown').addEventListener('click', toggleAboutDropdown);
+document.getElementById('inspiration-dropdown').addEventListener('click', toggleInspirationDropdown);
+document.getElementById('contacts-box').addEventListener('click', toggleContactsDropdown);
